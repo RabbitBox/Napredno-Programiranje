fef281dba3fa6afe4b16a5eb976d3f3f086bc90e
RabbitBox <jovanov.aleksandar997@gmail.com>
2020-01-28 17:00:06 +0100

    stream API example(stream().map().reduce()

--- ZadaciOdAuditoriski/src/mk/ukim/finki/np/Test.java
+++ ZadaciOdAuditoriski/src/mk/ukim/finki/np/Test.java
@@ -1,9 +1,39 @@
 package mk.ukim.finki.np;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 public class Test {
 
     public static void main(String[] args) {
+        List<Integer> values = Arrays.asList(1,2,3,4,5,6);
 
-        System.out.println("asdasdasd");
+        //System.out.println( values.stream().map(i -> i * 2).reduce(0,((i, j) -> i + j)) );
+        /** we are trying to implement / explain this code, step by step */
+
+        Function<Integer, Integer> f = new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer integer) {
+                return integer*2;
+            }
+        };
+
+        BinaryOperator<Integer> b = new BinaryOperator<Integer>() {
+            @Override
+            public Integer apply(Integer i, Integer j) {
+                return i + j;
+            }
+        };
+
+        Stream<Integer> s = values.stream();
+
+        Stream<Integer> s1 = s.map(f);
+
+        Integer result = s1.reduce(0,b);
+        System.out.println(result);
+
     }
 }
